ef06cb789634da12490fefd1be56416d78a1a88c
Flavio Milani <milanidesign7@gmail.com>
2020-05-26 16:35:20 -0300

    projeto classificados em desenvolvimento

---
+++ add-anuncio.php
@@ -0,0 +1,13 @@
+<?php require('inc/header.php'); ?>
+<?php require('session.php'); ?>
+<div class="container">
+<h1>Meus Anúncios</h1>
+
+<a href="add-anuncio.php">Adicionar Anúncio</a>
+
+
+
+</div>
+
+
+<?php require('inc/footer.php'); ?>
